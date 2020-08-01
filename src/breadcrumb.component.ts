f4fc0edbbda8089a57fb642a71d5cead8ca6340a
Arpit Yadav <arpit.absyadav@yahoo.com>
2020-08-01 22:25:13 +0530

    breadcrum selector chnaged to abs-breadcrumbs

--- src/breadcrumb.component.ts
+++ src/breadcrumb.component.ts
@@ -4,30 +4,37 @@ import { Breadcrumb } from './breadcrumb';
 import { BreadcrumbService } from './breadcrumb.service';
 
 @Component({
-    // tslint:disable-next-line:component-selector
-    selector: 'breadcrumb',
-    template:
-    `<div #template>
-    <ng-content></ng-content>
-</div>
-<div class="container" *ngIf="template.children.length == 0">
-    <div class="nav-wrapper">
-        <div class="breadcrumb" *ngFor="let route of breadcrumbs" [ngClass]="{'last': route.terminal}">
-            <!-- disable link of last item -->
-            <a href="" *ngIf="!route.terminal" [routerLink]="[route.url]">{{ route.displayName }}</a>
-            <span *ngIf="route.terminal">{{ route.displayName }}</span>
-        </div>
+  // tslint:disable-next-line:component-selector
+  selector: 'abs-breadcrumb',
+  template: `<div #template>
+      <ng-content></ng-content>
     </div>
-</div>`
+    <div class="container" *ngIf="template.children.length == 0">
+      <div class="nav-wrapper">
+        <div
+          class="breadcrumb"
+          *ngFor="let route of breadcrumbs"
+          [ngClass]="{ last: route.terminal }"
+        >
+          <!-- disable link of last item -->
+          <a href="" *ngIf="!route.terminal" [routerLink]="[route.url]">{{
+            route.displayName
+          }}</a>
+          <span *ngIf="route.terminal">{{ route.displayName }}</span>
+        </div>
+      </div>
+    </div>`,
 })
 export class BreadcrumbComponent {
-    breadcrumbs: Breadcrumb[];
+  breadcrumbs: Breadcrumb[];
 
-    constructor(private breadcrumbService: BreadcrumbService) {
-        breadcrumbService.breadcrumbChanged.subscribe((crumbs: Breadcrumb[]) => { this.onBreadcrumbChange(crumbs); });
-    }
+  constructor(private breadcrumbService: BreadcrumbService) {
+    breadcrumbService.breadcrumbChanged.subscribe((crumbs: Breadcrumb[]) => {
+      this.onBreadcrumbChange(crumbs);
+    });
+  }
 
-    private onBreadcrumbChange(crumbs: Breadcrumb[]) {
-        this.breadcrumbs = crumbs;
-    }
+  private onBreadcrumbChange(crumbs: Breadcrumb[]) {
+    this.breadcrumbs = crumbs;
+  }
 }
